6f96f7c43b5ebb0b3f68e66ba4f320c8e21f03b1
FaizAP412 <faiz.pribadi@gmail.com>
2024-10-02 14:11:12 +0700

    tutorial 5 done

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from main.views import show_main, create_mood_entry, login_user, register, show_xml, show_json, show_xml_by_id, show_json_by_id
-from main.views import logout_user, delete_mood, edit_mood
+from main.views import logout_user, delete_mood, edit_mood, add_mood_entry_ajax
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
+    path('create-mood-entry-ajax', add_mood_entry_ajax, name='add_mood_entry_ajax'),
     path('create-mood-entry', create_mood_entry, name='create_mood_entry'),
     path('delete/<uuid:id>', delete_mood, name='delete_mood'), # sesuaikan dengan nama fungsi yang dibuat
     path('edit-mood/<uuid:id>', edit_mood, name='edit_mood'),
